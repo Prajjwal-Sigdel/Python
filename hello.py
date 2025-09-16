04e8c1574a6b09552160584e5ca8bba3d90a3686
Prajjwal-Sigdel <prajjwal410@gmail.com>
2025-09-16 14:56:57 +0545

    Learning Print function

--- hello.py
+++ hello.py
@@ -1,3 +1,12 @@
-print("Hello world")
-a = input("What is your name ? ")
-print("Have a great day" + "Hope you have a great day", a)
+# Getting input from the user
+name = input("What is you name? ")
+
+# Printing normally using Python
+print("Hello, World")
+
+# Manipulating named Parameters of Print functions
+print("Hello, ", end="\n\n")  # What to put at the end of the print function
+print("world")
+
+# What to put after every parameter seperations
+print("Welcome to the chat,", name, sep="___")
