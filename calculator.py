754a4b2171b6e2ba8855a322e4807fc79a4df36a
Prajjwal-Sigdel <prajjwal410@gmail.com>
2025-09-19 17:10:28 +0545

    Function and Variable basic info done

---
+++ calculator.py
@@ -0,0 +1,15 @@
+# accepting two values for calculation
+x = int(input("Enter a number : "))
+y = int(input("Enter another number : "))
+
+# Addition
+print(x + y)
+
+# subtraction
+print(x - y)
+
+# Multiplication
+print(x * y)
+
+# Division
+print(x / y)
--- hello.py
+++ hello.py
@@ -1,6 +1,15 @@
 # Getting input from the user
 name = input("What is you name? ")
 
+# remove whitespace of the string
+name = name.strip()
+
+# Capitalize the users name
+name = name.capitalize()
+
+# Split users name into first and second
+first, last = name.split(" ")
+
 # Printing normally using Python
 print("Hello, World")
 
@@ -9,4 +18,7 @@ print("Hello, ", end="\n\n")  # What to put at the end of the print function
 print("world")
 
 # What to put after every parameter seperations
-print("Welcome to the chat,", name, sep="___")
+print("Welcome to the chat,", last, sep="___")
+
+# Printing formatted string
+print(f"Hello , {first}")
