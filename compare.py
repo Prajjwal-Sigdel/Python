b873b2c1c1b4276981afde0300e0c6d1cde881ea
Prajjwal-Sigdel <prajjwal410@gmail.com>
2025-09-20 14:53:12 +0545

    Completed Conditionals

--- compare.py
+++ compare.py
@@ -51,4 +51,25 @@ def main():             # Main function that does the job
         print("The number is odd...")
 
 
-main()
+def mat():              # Match function to know the day of the week
+    a = int(input("Enter the day number: "))
+    match a:
+        case 1:
+            print("Sunday")
+        case 2:
+            print("Monday")
+        case 3:
+            print("Tuesday")
+        case 4:
+            print("Wednesday")
+        case 5:
+            print("Thrusday")
+        case 6:
+            print("Friday")
+        case 7:
+            print("Saturday")
+        case _:
+            print("Invalid day number")
+
+
+mat()
